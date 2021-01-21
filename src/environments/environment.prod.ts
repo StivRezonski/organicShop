4d0ec0cdcae5d59ab60db4c552dbee120ccba10b
stivrezonski <alexkovach@hotmail.com>
2021-01-21 23:19:54 +0100

    login page

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyD56qe_64iszN87YHSP5xoyze_Fl8rdPmU',
+    authDomain: 'oshop-b2d2d.firebaseapp.com',
+    projectId: 'oshop-b2d2d',
+    storageBucket: 'oshop-b2d2d.appspot.com',
+    messagingSenderId: '8273218313',
+    appId: '1:8273218313:web:38e627cebe1201c11f21d8',
+    measurementId: 'G-9TTYQW7ZRP'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyD56qe_64iszN87YHSP5xoyze_Fl8rdPmU',
+    authDomain: 'oshop-b2d2d.firebaseapp.com',
+    projectId: 'oshop-b2d2d',
+    storageBucket: 'oshop-b2d2d.appspot.com',
+    messagingSenderId: '8273218313',
+    appId: '1:8273218313:web:38e627cebe1201c11f21d8',
+    measurementId: 'G-9TTYQW7ZRP'
+  }
 };
 
 /*
